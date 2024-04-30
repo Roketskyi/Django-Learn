53335947f24f9eaafb52f00b060a549cbc186a63
Roman <romanroketskiy05@gmail.com>
2024-04-30 09:48:18 +0300

    update mode.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import User, News
+from .models import News
 
-admin.site.register(User)
 admin.site.register(News)
--- main/models.py
+++ main/models.py
@@ -1,34 +1,5 @@
 from django.db import models
 
-class User(models.Model):
-    login = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField()
-    role = models.IntegerField()
-
-    def __str__(self):
-        return self.login
-    
-    class Meta:
-        verbose_name = 'Користувача'
-        verbose_name_plural = 'Користувачі'
-
-
-class News(models.Model):
-    title = models.CharField(max_length=255)
-    byte_content = models.TextField()  # Текст без форматування
-    html_content = models.TextField()  # HTML-форматований текст новини
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='news_images/')
-
-    def __str__(self):
-        return self.title
-    
-    class Meta:
-        verbose_name = 'Новину'
-        verbose_name_plural = 'Новини'
-
 class Base(models.Model):
     id = models.AutoField(primary_key=True)
     login = models.CharField(max_length=100)
@@ -38,3 +9,18 @@ class Base(models.Model):
 
     def __str__(self):
         return self.login
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    byte_content = models.TextField()  # Текст без форматування
+    html_content = models.TextField()  # HTML-форматований текст новини
+    author = models.ForeignKey(Base, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='news_images/')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Новину'
+        verbose_name_plural = 'Новини'
